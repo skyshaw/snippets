f2a34caa008b3cd8a2b8aa53102d54e3497f1daa
skyshaw <pxtian2008@gmail.com>
2012-01-16 18:35:41 +0800

    repalce tab with space

--- get_img_from_html.py
+++ get_img_from_html.py
@@ -1,38 +1,38 @@
 #-*- coding:utf-8 -*-
 import urllib2, sys, re, urllib
 def get_page(url):
-	try:
-		fileHandle = urllib2.urlopen(url)
-		string = fileHandle.read()
-		fileHandle.close()
-	except IOError:
-		print 'Cannot open URL %s for reading' % url
-		string = ''
-	return string
+    try:
+        fileHandle = urllib2.urlopen(url)
+        string = fileHandle.read()
+        fileHandle.close()
+    except IOError:
+        print 'Cannot open URL %s for reading' % url
+        string = ''
+    return string
 
 
 def get_file_name(url):
-	return url[url.rfind('/') + 1 : ]
+    return url[url.rfind('/') + 1 : ]
 
 def get_extension_name(url):
-	return url[url.rfind('.') : ]
+    return url[url.rfind('.') : ]
 
 def get_images(url):
-	string = get_page(url)
-	regular = r'<img([ ]*)src="(.*?)"(.*)/>'
-	li = re.findall(regular, string)
-	num = 0
-	for img_link in li:
-		try:
-			urllib.urlretrieve(img_link[1], str(num) + get_extension_name(img_link[1]))
-			print  "%s --> %s finished" % (img_link[1], str(num) + get_extension_name(img_link[1]))
-			num += 1
-		except IOError:
-			print "%s download failed" % img_link[1]
+    string = get_page(url)
+    regular = r'<img([ ]*)src="(.*?)"(.*)/>'
+    li = re.findall(regular, string)
+    num = 0
+    for img_link in li:
+        try:
+            urllib.urlretrieve(img_link[1], str(num) + get_extension_name(img_link[1]))
+            print  "%s --> %s finished" % (img_link[1], str(num) + get_extension_name(img_link[1]))
+            num += 1
+        except IOError:
+            print "%s download failed" % img_link[1]
 
 if __name__ == "__main__":
-	if len(sys.argv) != 2:
-		print 'format error, input an url!'
-	else:
-		get_images(sys.argv[1])
-		
+    if len(sys.argv) != 2:
+        print 'format error, input an url!'
+    else:
+        get_images(sys.argv[1])
+        
--- rename.py
+++ rename.py
@@ -9,27 +9,27 @@ os.chdir(work_dir)
 #it only supports removing some prefix in filename
 #then  set the prefix you wanto to remove
 if sys.argv[1] == "-d":
-	rm_str = raw_input("input the string you want to remove:")
-	for filename in os.listdir(work_dir):
-		if os.path.isfile(filename):
-		    old = filename
-		    new = filename.lstrip(rm_str)
-		    if old != new:
-		        os.rename(old, new)
+    rm_str = raw_input("input the string you want to remove:")
+    for filename in os.listdir(work_dir):
+        if os.path.isfile(filename):
+            old = filename
+            new = filename.lstrip(rm_str)
+            if old != new:
+                os.rename(old, new)
 #rename all the files by name 1,2,3...
 elif sys.argv[1] == "-l":
-	for filename in os.listdir(work_dir):
-		if os.path.isfile(filename):
-			old = filename
-			new = "prefix"+filename
-			os.rename(old, new)
-	cnt = 0
-	for filename in os.listdir(work_dir):
-		if os.path.isfile(filename):
-			old = filename
-			new = str(cnt) + os.path.splitext(old)[1]
-			cnt += 1
-			os.rename(old, new)
-		
+    for filename in os.listdir(work_dir):
+        if os.path.isfile(filename):
+            old = filename
+            new = "prefix"+filename
+            os.rename(old, new)
+    cnt = 0
+    for filename in os.listdir(work_dir):
+        if os.path.isfile(filename):
+            old = filename
+            new = str(cnt) + os.path.splitext(old)[1]
+            cnt += 1
+            os.rename(old, new)
+        
 
 
