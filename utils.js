82bc3e9e09b8bae3b5593e7fa89df7fcab0af614
skyshaw <pxtian2008@gmail.com>
2012-01-29 15:02:21 +0800

    modify utils.js

--- utils.js
+++ utils.js
@@ -30,21 +30,3 @@ function addClass(element, value) {
     }
 }
 
-function highlightPage() {
-    if (!document.getElementsByTagName) return false;
-    if (!document.getElementById) return false;
-    if (!document.getElementById("navigation")) return false;
-    var nav = document.getElementById("navigation");
-    var links = nav.getElementsByTagName("a");
-    for (var i = 0; i < links.length; ++i) {
-        var linkurl = links[i].getAttribute("href");
-        var current = window.location.href;
-        if (current.indexOf(linkurl) != -1) {
-            links[i].className = "here";
-            var linktext = links[i].lastChild.nodeValue.toLowerCase();
-            document.body.setAttribute("id", linktext);
-        }
-    }
-}
-
-addLoadEvent(highlightPage);
