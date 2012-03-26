d79b710ce830431d49132d3e4b550c08e80f8b24
skyshaw <pxtian2008@gmail.com>
2012-03-26 22:14:49 +0800

    m

--- c-cpp/misc/algo/findof1.cpp
+++ c-cpp/misc/algo/findof1.cpp
@@ -8,17 +8,17 @@ using namespace std;
 int main() {
   vector<int> coll; 
   INSERT_ELEMENTS(coll, 1, 11);
-  vector<int> subcoll;
-  INSERT_ELEMENTS(subcoll, 3, 5);
+  vector<int> searchcoll;
+  INSERT_ELEMENTS(searchcoll, 3, 5);
   PRINT_ELEMENTS(coll);
-  PRINT_ELEMENTS(subcoll);
+  PRINT_ELEMENTS(searchcoll);
   vector<int>::iterator pos;
   pos = find_first_of(coll.begin(), coll.end(),
-                      subcoll.begin(), subcoll.end());
+                      searchcoll.begin(), searchcoll.end());
   cout << distance(coll.begin(), pos) + 1 << endl;
   vector<int>::reverse_iterator rpos;
   rpos = find_first_of(coll.rbegin(), coll.rend(),
-                       subcoll.begin(), subcoll.end());
+                       searchcoll.begin(), searchcoll.end());
   cout << distance(coll.begin(), rpos.base()) << endl;
   return 0;
 }
