bc0686054d40c2d85280d42e669a4dc37f861485
skyshaw <pxtian2008@gmail.com>
2012-03-28 20:22:13 +0800

    refine autowiki

--- autowiki.py
+++ autowiki.py
@@ -2,16 +2,16 @@ import os
 import re
 
 def get_item_lists(filename):
-    f = open(filename, "r")
+    file_ = open(filename, "r")
     item_list = []
     while True:
-        line = f.readline()
+        line = file_.readline()
         if not line:
             break
-        m = re.match(r'#\s+\[\[\./(.+)\|.+\]\]', line)
-        if m is not None:
-            item_list.append(m.group(1))
-    f.close()
+        title = re.match(r'#\s+\[\[\./(.+)\|.+\]\]', line)
+        if title is not None:
+            item_list.append(title.group(1))
+    file_.close()
     return item_list
 
 def add_links():
@@ -20,14 +20,15 @@ def add_links():
     for item in os.listdir("."):
         if (os.path.isfile(item)) and (item != "index.wiki") and \
             (item.endswith(".wiki")) and (item[:-5] not in item_list):
-            f = open(item, 'r')
-            title = f.readline()
-            m = re.match(r'=+([^=]+)=+', title)
-            if m is None:
+            file_ = open(item, 'r')
+            line = file_.readline()
+            title = re.match(r'=+([^=]+)=+', line)
+            if title is None:
                 continue
-            print m.group(1)
-            index_page.write('# [[./' + item[:-5] + '|' + m.group(1) + ']]\n')
-            f.close()
+            print title.group(1)
+            index_page.write('# [[./' + item[:-5] + '|'
+                             + title.group(1) + ']]\n')
+            file_.close()
     index_page.close()
 
 if __name__ == "__main__":
