2996335e3004c5bdc6b5174f015ec24d8be0cdbd
skyshaw <pxtian2008@gmail.com>
2012-05-09 20:53:15 +0800

    refine preview.sh

--- sh/preview.sh
+++ sh/preview.sh
@@ -5,13 +5,15 @@ USAGE="usage: preview.sh [option] [filename]
               -m preview markdown file
               -h preview html file"
 
-
-if [[ "$1" == "-m" ]]; then
-    echo here
-    TMPFILE=$(mktemp) && \
-    pandoc -o $TMPuILE -t html -f markdown $2 && $BROWSER $TMPFILE 
-elif [[ "$1" == "-h" ]]; then
-    $BROWSER $2
-else
-    echo "$USAGE"
-fi
+case "$1" in
+"-m" ) (TMPFILE=$(mktemp) && \
+        pandoc -o $TMPFILE -t html -f markdown $2 && $BROWSER $TMPFILE)
+       ;;
+"-r" ) (TMPFILE=$(mktemp) && \
+        pandoc -o $TMPFILE -t html -f rst $2 && $BROWSER $TMPFILE)
+       ;;
+"-h" ) $BROWSER $2
+       ;;
+*)     echo "$USAGE"
+       ;;
+esac
